5c6dde91919dbf1924bbee7e7183ce2feebb6e81
Gaberu <gabrielrosadn@gmail.com>
2018-09-30 19:12:57 -0300

    develop Add create console implementation
    - now we can create consoles, all CRUD is ready for console
    
    Signed-off-by: Gaberu <gabrielrosadn@gmail.com>

--- MyGames/Application/Modules/ConsoleForm/ConsoleFormViewController.swift
+++ MyGames/Application/Modules/ConsoleForm/ConsoleFormViewController.swift
@@ -12,6 +12,12 @@ class ConsoleFormViewController: UIViewController {
     
     public static let NIB_NAME = "ConsoleFormViewController"
     
+    @IBOutlet weak var titlePlacehoderLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var titleErrorLabel: UILabel!
+    @IBOutlet weak var manufacturerPlaceholderLabel: UILabel!
+    @IBOutlet weak var manufacturerTextField: UITextField!
+    
     lazy var presenter: ConsoleFormPresenterContract = {
         return ConsoleFormPresenter(view: self,
                                     saveConsole: InjectionUseCase.provideSaveConsole())
@@ -33,23 +39,45 @@ class ConsoleFormViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setCurrentConsole()
+        setupLabels()
     }
     
     @objc func didSaveTapped() {
-        let consoleToBeSaved = Console(guid: guid,
-                                       title: "",
-                                       manufacturer: "")
         
-        presenter.save(console: consoleToBeSaved)
-        navigationController?.popViewController(animated: true)
+        if isFieldsvalidated() {
+            titleErrorLabel.alpha = 0
+            let consoleToBeSaved = Console(guid: guid,
+                                           title: titleTextField.text ?? "",
+                                           manufacturer: manufacturerTextField.text ?? "")
+            
+            presenter.save(console: consoleToBeSaved)
+            navigationController?.popViewController(animated: true)
+        } else {
+            titleErrorLabel.alpha = 1
+        }
+        
+    }
+    
+    private func isFieldsvalidated() -> Bool {
+        if let titleText = titleTextField.text {
+            return !titleText.isEmpty
+        }
+        
+        return false
+    }
+    
+    private func setupLabels() {
+        titlePlacehoderLabel.text         = "Title"
+        manufacturerPlaceholderLabel.text = "Manufacturer"
     }
 }
 
 extension ConsoleFormViewController: ConsoleFormViewContract {
     func setCurrentConsole() {
         if let currentConsole = console {
-            guid = currentConsole.guid
-            //            fill all fields
+            guid                       = currentConsole.guid
+            titleTextField.text        = currentConsole.title
+            manufacturerTextField.text = currentConsole.manufacturer
         }
     }
 }
